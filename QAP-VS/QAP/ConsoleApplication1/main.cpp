7fb4f242a384400095d7597127a804b51a3cb631
YruiGN <yruigneris@gmail.com>
2017-08-17 13:19:50 -0300

    Iniciando VS

---
+++ QAP-VS/QAP/ConsoleApplication1/main.cpp
@@ -0,0 +1,16 @@
+#include "bibliotecas.h"
+
+int main()
+{
+
+	int i;
+	printf("Entre");
+	scanf("%d", &i);
+
+	printf("FIM");
+
+
+	printf("Deseja sair");
+	scanf("%d", &i);
+	return 0;
+}
--- QAP/main.cpp
+++ QAP/main.cpp
@@ -1,3 +1,8 @@
+#include "bibliotecas.h"
+#include "calculaDistancia.h"
+
 int main(){
 	return 0;
+
+
 }
